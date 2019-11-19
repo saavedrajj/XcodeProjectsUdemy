502a71a7f629e515548c06b69d255c39a83d6a04
Juan Saavedra <saavedrajj@gmail.com>
2019-11-19 22:49:07 +0000

    s

--- Playgrounds/Optionals.playground/Contents.swift
+++ Playgrounds/Optionals.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var str = "Hello, playground"
+var myName : String?
+myName?.uppercased()
+    
+// optionals ? vs !
+var myAge = "f"
+
+var myInteger = (Int(myAge) ?? 0 ) * 5
+
+if let myNumber = Int(myAge) {
+    print(myNumber * 5)
+} else {
+    print("wrong input")
+}
